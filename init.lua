6a87063a7794b9136257b0b00ecf6e2dfd80291e
Colby Noah <colbynoah@gmail.com>
2026-05-08 18:07:07 -0700

    Added colorcolumn at 80 chars

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ require("config.options")
 require("config.remap")
 require("config.diagnostics")
 
+vim.opt.colorcolumn = "80"
+
 -- Fix floating window background + border halo
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
